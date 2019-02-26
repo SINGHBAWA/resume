ac9b512e6427b10c0ae21bbd2d425c193063d5d0
Aman preet Singh <amansinghbawa@gmail.com>
2019-02-26 20:04:05 +0530

    deny x frame options

--- resume_parser/settings.py
+++ resume_parser/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'resume_parser.urls'
@@ -73,7 +73,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'resume_parser.wsgi.application'
 
-X_FRAME_OPTIONS = 'DENY'
+# X_FRAME_OPTIONS = 'DENY'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
